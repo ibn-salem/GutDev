b73d537f8da9b79dae1d469f4095bd102a0a32f3
ibn-salem <jonas.ibn-salem@gmx.de>
2016-11-02 15:32:01 +0100

    udpate run_all_analysis.sh script

--- bash/run_all_analysis.sh
+++ bash/run_all_analysis.sh
@@ -8,14 +8,14 @@
 # download liftOver tool from USCS and chain files to convert mm10 cooridnates to mm9
 
 #~ # UCSC liftover chains
-#~ mkdir -p data/UCSC
-#~ wget -P data/UCSC http://hgdownload.cse.ucsc.edu/goldenPath/mm10/liftOver/mm10ToMm9.over.chain.gz
-#~ gunzip data/UCSC/*.gz
+mkdir -p data/UCSC
+wget -P data/UCSC http://hgdownload.cse.ucsc.edu/goldenPath/mm10/liftOver/mm10ToMm9.over.chain.gz
+gunzip data/UCSC/*.gz
 
 #~ # download liftOver tool from UCSC:
-#~ mkdir bin
-#~ wget -P bin http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/liftOver
-#~ chmod u+x bin/liftOver
+mkdir bin
+wget -P bin http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/liftOver
+chmod u+x bin/liftOver
 
 #-----------------------------------------------------------------------
 # convert introns from mm10 to mm9
@@ -24,8 +24,6 @@ for F in data/Intron_Retention_bed_files/*/*.bed ; do
 	
 	echo "File -> $F"
 	
-		#~ | awk -F"\t" '{if ($4 == "+") {print $1,$2,$3,".","0",$4} else {print $1,$3,$2,".","0",$4} }' OFS="\t" \
-
 	# convert into standard .bed file
 	cat ${F} \
 		| awk -F"\t" '{if ($3 >= $2) {print $1,$2,$3} else {print $1,$3,$2} }' OFS="\t" \
@@ -40,12 +38,14 @@ for F in data/Intron_Retention_bed_files/*/*.bed ; do
 done
 
 
-
-#-----------------------------------------------------------------------
-# Run R script to convert .xlsx tables to standart bed files
-#~ Rscript l
+# create results directory
+mkdir -p results
 
 
 #=======================================================================
-# Analyse Overlap
+# Analyse overlap of retained and all introns with chromatin mark peaks
 #=======================================================================
+
+Rscript R/run_gutDev_analysis.R
+
+
